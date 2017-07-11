946cc77f89e4d8b7401f744d1e44f39e05c9be7b
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-11 16:00:26 -0600

    updated styles.  Changed text color to white.

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <!DOCTYPE HTML>
 <html>
 	<head><title>A Little About Alex</title>
-	<link rel="stlyesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 		<body>
 			<p>
-				This is, sadly, me. <br><img src="images/alex.jpg" alt="Alex"><br>
+				This is me. <br><img src="images/alex.jpg" alt="Alex">
 			</p>
 			<p>
 				While I was born in New Mexico, I have spent the better part of my life in California.  While I still
 				lived there, I graduated from K-12 and eventually found my way into the University of California,
-				Irvine.  In spite of myself, I managed to earn double bachelors' of arts in psychology and studio arts.
-				After a number missteps and failures, I found myself stuck in New Mexico.  But that was then, and this is
-				now; and now is the time to move up and move on.
+				Irvine.  I graduated with a double bachelors' of arts in psychology and studio arts, and my specialty is painting
+				circus clowns.  Now I am back in New Mexico, and ready to move onward and upward.
 			</p>
 			<p>
-				Among other things, I like fountain pens and video games.  The types of games I like run the gamut from
-				slow, thoughtful puzzle games to face-paced action games.  <br>
-				Currently inked: Lamy 2000 with KWZ Red #3<br>
-				Currently dying to: Dark Souls 2: Scholar of the First Sin.
-			</p>
+				Among other things, I like fountain pens and video games. My favorite store is Santa Fe pens, which has managed
+				to survive in spite of all the ups and downs in the economy.  The types of games I like run the gamut from
+				slow, thoughtful puzzle games to face-paced action games.</p>
+			<p>Currently inked: Lamy 2000 with KWZ Red #3</p>
+				<p>Currently dying to: Dark Souls 2: Scholar of the First Sin.</p>
 	</body>
 </html>

f20406bc6b4291940e2779ce9eff7834816a1eaf
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-18 15:17:58 -0600

    Added detail to quantitative section per suggestion

--- prodev.php
+++ prodev.php
@@ -9,9 +9,12 @@
 		<header>Three Things About Me</header>
 			<h1>Quantitative</h1>
 			<ol>
-				<li>I earned two B.A.s from UCI (Psychology and Studio Arts)</li>
-				<li>I can play the violin (but I haven’t in a long time)</li>
-				<li>I was born in NM, but I grew up in CA</li>
+				<li>From 2004 to 2007, I attended the University of California, Irvine where I earned two B.A.s
+					(in Psychology and Studio Arts).</li>
+				<li>I started playing the violin when I was about 7 years old and played until I graduated from high
+					school (but I haven't played much since starting college).</li>
+				<li>My mother came back to NM to give birth to me, but we were back in CA after two weeks; from then
+					until August 2012, I grew up and lived in various places around CA, mostly the high desert regions.</li>
 			</ol>
 
 			<h1>Qualitative</h1>

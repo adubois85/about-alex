175eee813dc341a5b607d44891f5f2aa7eb269f2
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-18 01:07:18 -0600

    Added qualitative / quantitative list to prodev

--- prodev.php
+++ prodev.php
@@ -6,6 +6,29 @@
 		<title>Professional Development</title>
 	</head>
 	<body>
-		<p></p>
+		<header>Three Things About Me</header>
+			<h1>Quantitative</h1>
+			<ol>
+				<li>I earned two B.A.s from UCI (Psychology and Studio Arts)</li>
+				<li>I can play the violin (but I haven’t in a long time)</li>
+				<li>I was born in NM, but I grew up in CA</li>
+			</ol>
+
+			<h1>Qualitative</h1>
+			<ol>
+				<li>Detail-oriented</li>
+				<li>Usually quiet</li>
+				<li>Can be a bit slow at the start of something new, but pick-up quickly from there</li>
+			</ol>
+
+			<h1>My career buddy is Augustina.</h1>
+
+		<header>Golden Circles</header>
+			<h1>First</h1>
+			<ul>
+				<li>Why: </li>
+				<li>How: </li>
+				<li>What: </li>
+			</ul>
 	</body>
 </html>

657dbee4e3c0641a681d2450be10403a3a3d6d8d
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-18 01:37:25 -0600

    Added alternate golden circle

--- prodev.php
+++ prodev.php
@@ -21,14 +21,31 @@
 				<li>Can be a bit slow at the start of something new, but pick-up quickly from there</li>
 			</ol>
 
-			<h1>My career buddy is Augustina.</h1>
+			<h2>My career buddy is Augustina.</h2>
 
 		<header>Golden Circles</header>
-			<h1>First</h1>
+			<h1>Main Golden Circle</h1>
 			<ul>
 				<li>Why: </li>
 				<li>How: </li>
 				<li>What: </li>
 			</ul>
+
+			<h2>Elevator Pitch</h2>
+				<p>
+					Hello, my name is Alex DuBois.
+				</p>
+
+			<h1>Alternate Golden Circle</h1>
+			<ul>
+				<li>Why: Create memorable interactive experiences.</li>
+				<li>How: Through my coding skills and experience playing video games</li>
+				<li>What: Developing video games</li>
+			</ul>
+
+			<h2>Elevator pitch</h2>
+				<p>
+					Hello, my name is Alex DuBois.
+				</p>
 	</body>
 </html>
